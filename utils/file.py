af7ce0e43e1435cb9e2c1ef563128771cb254b35
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-12-01 23:05:26 +0545

    Added file helper

---
+++ utils/file.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+
+def check_size(resource, max_size):
+    """
+    Serializer validator
+    Validates file size
+    Raises serializer validation error if requirement does not match
+    """
+    if resource.size / 1000 > max_size:
+        raise serializers.ValidationError(
+            f"Resource exceeds maximum upload size."
+            f" Allowed maximum size: {max_size / 1000} MB")
