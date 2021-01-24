ab810230d1c84737ce426c7fd6c1c96f0be9c191
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-01-24 10:56:01 +0545

    Added name and ingredients contains filter

---
+++ item/filters.py
@@ -0,0 +1,12 @@
+import django_filters
+
+from item.models import MenuItem
+
+
+class MenuItemFilter(django_filters.FilterSet):
+    class Meta:
+        model = MenuItem
+        fields = {
+            'name': ['contains'],
+            'ingredients': ['contains']
+        }
