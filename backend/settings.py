2da08477267d201eddee54de5c446fb9e77440fe
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-22 22:12:40 +0545

    Added filter and search backend

--- backend/settings.py
+++ backend/settings.py
@@ -81,7 +81,11 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 15,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+    ],
 }
 
 MIDDLEWARE = [
