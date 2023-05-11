c41747677cfcce7247d445ffe7654cdbf5b2605d
Parajuli Kiran <kiranparajuli589@gmail.com>
2023-05-11 15:09:18 +0545

    Update the production serve code
    Signed-off-by: Parajuli Kiran <kiranparajuli589@gmail.com>

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,7 @@
 workers = 4
 max_requests = 1000
 timeout = 30
-bind = "0.0.0.0:8001"
+bind = "0.0.0.0:8002"
 preload_app = True
-accesslog = "/var/log/gunicorn/foodswipe/access.log"
-errorlog = "/var/log/gunicorn/foodswipe/error.log"
+accesslog = "/home/ubuntu/dev/foodswipe/BackEnd/logs/gunicorn/access.log"
+errorlog = "/home/ubuntu/dev/foodswipe/BackEnd/logs/gunicorn/error.log"
